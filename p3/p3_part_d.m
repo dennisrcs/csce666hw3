a067bd84321b22330ee63b84477ac0dabad21fa0
Dennis <denisrcs@gmail.com>
2016-10-27 16:36:07 -0500

    question 3 finished

--- p3/p3_part_d.m
+++ p3/p3_part_d.m
@@ -8,7 +8,10 @@ img = reshape(fullimg, [width*height, 3]);
 img_size = size(img, 1);
 clusters = zeros(img_size, 1);
 
-figure;
+res_ind = 1:2:img_size;
+img = img(res_ind,:);
+img_size = length(img);
+sse = zeros(1,10);
 
 % k-means
 for k = 1:10
@@ -19,16 +22,16 @@ for k = 1:10
         pixels_set = img(1+(i-1)*num_px_set:i*num_px_set,:);
         cent(i,:) = mean(pixels_set);
     end
-    
+
     % assigning random clusters
     for i = 1:img_size
         clusters(i) = randi(k);
     end
-    
+
     old_cent = cent + 1;
     while ~isequal(cent, old_cent)
         old_cent = cent;
-        
+
         % Reassign each example to the cluster with the nearest mean
         distances = zeros(img_size, k+1);
         for j = 1:k
@@ -36,22 +39,24 @@ for k = 1:10
         end
         distances(:,k+1) = Inf(img_size,1);
         [ignore, clusters] = min(distances,[],2);
-        
+
         % compute the sample mean of each cluster
         for j = 1:k
             samples = img(clusters == j, :);
             cent(j,:) = mean(samples);
         end
     end
-    
-    new_image = zeros(width*height, 3);
+
+    new_image = zeros(img_size, 3);
     % assigning random clusters
     for i = 1:img_size
         new_image(i,:) = cent(clusters(i),:);
     end
-    
-    new_image = reshape(new_image, [width, height, 3]);
-    subplot(5,2,k);
-    imagesc(uint8(new_image));
-    title(strcat('K=',num2str(k)));
+
+    % computing sum squared error
+    sse(k) = sum(sum((new_image-double(img)).^2,2));
 end
+
+figure, semilogy(sse, 'LineWidth', 2);
+axis([1 10 4*10^7 1.5*10^9]);
+xlabel('K'); ylabel('Sum Squared Error');
